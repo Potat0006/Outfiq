766caa477b50f747014d1f7f44780f6931d1d265
Lemons <crosszovers2018@gmail.com>
2026-06-04 10:36:15 +0700

    fix cors

--- api/delete-image.js
+++ api/delete-image.js
@@ -8,6 +8,27 @@ cloudinary.config({
 
 export default async function handler(req, res) {
 
+  // CORS
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  // Preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed"
@@ -19,7 +40,9 @@ export default async function handler(req, res) {
     const { publicId } = req.body;
 
     const result =
-      await cloudinary.uploader.destroy(publicId);
+      await cloudinary.uploader.destroy(
+        publicId
+      );
 
     return res.status(200).json(result);
 
